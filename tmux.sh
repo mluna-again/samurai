5a95832e094f8f02fa235d0a4c79c022a5e0af8a
Manuel Luna <manuellunada@gmail.com>
2025-10-11 00:02:43 -0600

    update tmux.sh script

--- tmux.sh
+++ tmux.sh
@@ -1,7 +1,29 @@
 #! /usr/bin/env bash
 
+die() {
+  echo "$*" 1>&2
+  exit 1
+}
+
+toggle_status() {
+  local status
+  status="$1"
+
+  if [ "$status" = on ]; then
+    tmux set -g status off || exit
+  else
+    tmux set -g status on || exit
+  fi
+}
+
+if [ -z "$TMUX" ]; then
+  die not running inside tmux
+fi
+
+current_status=$(tmux show-option -gv status) || exit
+trap "tmux set -g status \"$current_status\"" EXIT
+toggle_status "$current_status"
+
 session_name=$(tmux list-sessions -F '#{session_last_attached}@#{session_name}' | sort -r | awk -F@ '{printf "%s@%s\n", $2, $1}' | samurai) || exit
 
-[ -z "$session_name" ] && exit 0
-
-tmux switch-client -t "$session_name"
+echo "$session_name"
